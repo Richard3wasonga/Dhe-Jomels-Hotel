4fa015b29ac3ef93e8cdcd16b99772ed637517a5
richard <richard.wasonga@student.moringaschool.com>
2025-04-30 15:20:22 +0300

    enchanced menu appearance

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-const Menu = ({menu}) => {
+const Menu = ({ menu }) => {
+  
+  const groupedByCategory = menu.reduce((acc, item) => {
+    if (!acc[item.category]) {
+      acc[item.category] = [];
+    }
+    acc[item.category].push(item);
+    return acc;
+  }, {});
+
   return (
     <div>
-        <h2>Our menu</h2>
-        <ul>
-        {menu.map((item, index) => (
-          <li key={index}>
-            <strong>{item.name}</strong> - {item.price} ({item.category})
-          </li>
-        ))}
-      </ul>
-
+      <h2>Our Menu</h2>
+      {Object.entries(groupedByCategory).map(([category, items]) => (
+        <div key={category}>
+          <h3>{category}</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
