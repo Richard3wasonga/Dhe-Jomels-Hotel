a883270f1528bf754b27e4a49bd0d7574509a498
richard <richard.wasonga@student.moringaschool.com>
2025-04-30 16:48:14 +0300

    added styles for the menu nav

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilter } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
-const Options = ({toggleFilterBar,setsearchQuery,searchQuery}) => {
-   return ( 
-    <div>
-        <button onClick={toggleFilterBar}><FontAwesomeIcon icon={faFilter} /></button>
-        <input 
-          type="text" 
-          placeholder="Search for a dish..." 
-          value={searchQuery}
-          onChange={(e) => setsearchQuery(e.target.value)} 
-          className="menu-search"
-        />
+const Options = ({ toggleFilterBar, setsearchQuery, searchQuery }) => {
+  return (
+    <div className="options-container">
+      <button onClick={toggleFilterBar} className="filter-button">
+        <FontAwesomeIcon icon={faFilter} />
+      </button>
 
+      <div className="hotel-name">
+        <span className="hotel-title">Dhe Jomels</span>
+        <span className="hotel-subtitle">Hotel</span>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Search for a dish..."
+        value={searchQuery}
+        onChange={(e) => setsearchQuery(e.target.value)}
+        className="menu-search"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Options
+export default Options;
